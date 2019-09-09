e9036af5ef44695b556d6d14b9b6424e971cc02b
Sakshi <sgupta@ce.iitr.ac.in>
2019-09-09 12:22:06 +0530

    Web interface in flask
    index.html should in kept in directory named template which is in the same directory as app.py.

--- app.py
+++ app.py
@@ -1,39 +1,16 @@
+﻿from flask import Flask, request, render_template
+import base64 as b64
+app = Flask(__name__ , template_folder='template')
 
-from flask import Flask, render_template, request 
-  
-# Flask constructor takes the name of  
-# current module (__name__) as argument. 
-app = Flask(__name__, template_folder='template') 
-  
-# The route() function of the Flask class is a decorator,  
-# which tells the application which URL should call  
-# the associated function. 
-@app.route('/sakshi') 
-# ‘/’ URL is bound with hello_world() function. 
-# def hello_world(): 
-#     return 'Welcome'
-def welcomemessage():
-	return 'Fooling the Neural Network with Adversarial Attack'
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
 
-@app.route('/upload')
-def uploadfile():
-   return render_template('upload.html')
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        with open('sakshi.png', 'wb') as f:
+            data = b64.b64decode(request.form['data'])
+            f.write(data)
+        return 'Image Saved!'
 
-
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-	if request.method == 'POST':
-		f = request.files['file']
-		f.save(f.filename)
-		return 'file uploaded successfully'
-
-
-
-
-# main driver function 
-if __name__ == '__main__': 
-  
-    # run() method of Flask class runs the application  
-    # on the local development server. 
-    app.run() 

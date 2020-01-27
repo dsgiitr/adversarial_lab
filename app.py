14e4c86830dcdb201cefb1a51b75ce35f22e65ff
saswatpp <saswat.papp43@gmail.com>
2020-01-27 17:18:33 +0530

    Pages for few Attacks

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request, render_template, redirect, jsonify, Response
 import base64 as b64
 
 import commons
-from attack import fgsm_untargeted
+import attack
 import torch
 import cv2
 import numpy as np
@@ -14,65 +14,107 @@ from PIL import Image
 app = Flask(__name__ , template_folder='template')
 
 @app.route('/fgsm_untargeted', methods=['GET'])
-def hello_world():
+def fgsm():
     return render_template('fgsm.html')
 
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
-    if request.method == 'POST':
-        with open('images/sakshi.png', 'wb') as f:
-            data = b64.b64decode(request.form['data'])
-            f.write(data)
-        return 'Image Saved!'
+@app.route('/fgsm_targeted', methods=['GET'])
+def fgsm_targeted():
+    return render_template('fgsm_targeted.html')
+
+@app.route('/basic_iterative', methods=['GET'])
+def basic_iterative():
+    return render_template('basic_iterative.html')
+
+@app.route('/iterative_ll_class', methods=['GET'])
+def iterative_ll_class():
+    return render_template('iterative_ll_class.html')
 
 @app.route('/atk_fgsm_untargeted', methods=['POST'])
-def upload_file():
+def atk_fgsm_untargeted():
     if request.method == 'POST':
         if 'file' not in request.files:
             print("file not found")
             return redirect(request.url)
         file = request.files['file']
-        #epsilon = float(request.args.get('eps'))
         epsilon = float(request.form['eps'])
         model = request.form['model']
-        # if not file:
-        #     print('no file')
-        #     return 0
+        if not file:
+            print('no file')
+            return 0
         img_bytes = file.read()
-        top5_original, top5_perturbed, perturbed_image, original_image, perturbation = fgsm_untargeted( model, img_bytes, epsilon, device=torch.device('cpu'))
-        # print(original_image.dtype,"shape",original_image.shape)
+        top5_original, top5_perturbed, perturbed_image, original_image, perturbation = attack.fgsm_untargeted( model, img_bytes, epsilon, device=torch.device('cpu'))
         Image.fromarray((perturbed_image*255).astype('uint8')).save('perturbed_image.jpg')
         Image.fromarray((original_image*255).astype('uint8')).save('original.jpg')
         # cv2.imwrite('image1.jpg',original_image)
         # cv2.imshow("Image", original_image)
         # cv2.waitKey();
-        # per_buffer = cv2.imencode('.jpg', perturbed_image)[1]
-        # per_data = b64.b64encode(per_buffer).decode()
-        # perturbation_buffer = cv2.imencode('.jpg', perturbation)[1]
-        # perturbation_data = b64.b64encode(perturbation_buffer).decode()
-        # or_buffer = cv2.imencode('.jpg', original_image)[1]
-        # or_data = b64.b64encode(or_buffer).decode()
-
         or_data = commons.getb64str(original_image).decode()
         perturbation_data = commons.getb64str(perturbation).decode()
         per_data = commons.getb64str(perturbed_image).decode()
-        # print(b64.b64encode(or_data))
-        # or_data = or_data#.decode('utf8').replace("'",'"')
-        # perturbation_data = perturbation_data#.decode('utf8').replace("'",'"')
-        # per_data = per_data#.decode('utf8').replace("'",'"')
-        # im = Image.fromarray(original_image.astype("uint8"))
-        # rawBytes = io.BytesIO()
-        # im.save(rawBytes, "PNG")
-        # rawBytes.seek(0)
-        # with open('images/or_image.png', 'wb') as f:
-        #     data = rawBytes.read()
-        #     f.write(data)
         print('Success!')
-        # img = Image.fromarray(original_image.astype("uint8"))
-        # rawBytes = io.BytesIO()
-        # img.save(rawBytes, "JPEG")
-        # rawBytes.seek(0)
-        # img_base64 = base64.b64encode(rawBytes.read())
-        # return jsonify({'or_image':str(img_base64)})
         return jsonify({'original':top5_original,'perturbed':top5_perturbed, 'per_image':per_data, 'or_image':or_data, 'perturbation':perturbation_data})
 
+@app.route('/atk_fgsm_targeted', methods=['POST'])
+def atk_fgsm_targeted():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print("file not found")
+            return redirect(request.url)
+        file = request.files['file']
+        target = int(request.form['target'])
+        epsilon = float(request.form['eps'])
+        model = request.form['model']
+        if not file:
+            print('no file')
+            return 0
+        img_bytes = file.read()
+        top5_original, top5_perturbed, perturbed_image, original_image, perturbation = attack.fgsm_targeted( model, img_bytes, epsilon, target, device=torch.device('cpu'))
+        or_data = commons.getb64str(original_image).decode()
+        perturbation_data = commons.getb64str(perturbation).decode()
+        per_data = commons.getb64str(perturbed_image).decode()
+        print('Success!')
+        return jsonify({'original':top5_original,'perturbed':top5_perturbed, 'per_image':per_data, 'or_image':or_data, 'perturbation':perturbation_data})
+
+@app.route('/atk_basic_iterative', methods=['POST'])
+def atk_basic_iterative():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print("file not found")
+            return redirect(request.url)
+        file = request.files['file']
+        epsilon = float(request.form['eps'])
+        alpha = float(request.form['alpha'])
+        num_iter = int(request.form['num_iter'])
+        model = request.form['model']
+        if not file:
+            print('no file')
+            return 0
+        img_bytes = file.read()
+        top5_original, top5_perturbed, perturbed_image, original_image, perturbation = attack.basic_iterative( model, img_bytes, alpha, epsilon, num_iter, device=torch.device('cpu'))
+        or_data = commons.getb64str(original_image).decode()
+        perturbation_data = commons.getb64str(perturbation).decode()
+        per_data = commons.getb64str(perturbed_image).decode()
+        print('Success!')
+        return jsonify({'original':top5_original,'perturbed':top5_perturbed, 'per_image':per_data, 'or_image':or_data, 'perturbation':perturbation_data})
+
+@app.route('/atk_iterative_ll_class', methods=['POST'])
+def atk_iterative_ll_class():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print("file not found")
+            return redirect(request.url)
+        file = request.files['file']
+        epsilon = float(request.form['eps'])
+        alpha = float(request.form['alpha'])
+        num_iter = int(request.form['num_iter'])
+        model = request.form['model']
+        if not file:
+            print('no file')
+            return 0
+        img_bytes = file.read()
+        top5_original, top5_perturbed, perturbed_image, original_image, perturbation = attack.iterative_ll_class( model, img_bytes, alpha, epsilon, num_iter, device=torch.device('cpu'))
+        or_data = commons.getb64str(original_image).decode()
+        perturbation_data = commons.getb64str(perturbation).decode()
+        per_data = commons.getb64str(perturbed_image).decode()
+        print('Success!')
+        return jsonify({'original':top5_original,'perturbed':top5_perturbed, 'per_image':per_data, 'or_image':or_data, 'perturbation':perturbation_data})
--- attack.py
+++ attack.py
@@ -29,10 +29,11 @@ def fgsm_untargeted( model_inp, image_bytes, epsilon, device=torch.device('cpu')
 	del model
 	return dict(top5_original), dict(top5_perturbed), perturbed_image, original_image, perturbation
 
-def fgsm_targeted(model, image_bytes, epsilon, target, device=torch.device('cpu')):
+def fgsm_targeted(model_inp, image_bytes, epsilon, target, device=torch.device('cpu')):
+	model = get_model(model_inp)
 	tensor = transform_image(image_bytes=image_bytes)
 	#outputs = model.forward(tensor)
-	target = torch.tensor([target])
+	target = torch.tensor([target],dtype=torch.long)
 	original_output, perturbed_output, perturbed_image = GAE.fgsm_targeted(model, tensor, epsilon, target, device)
 	cpudev = torch.device('cpu')
 	original_output, perturbed_output = original_output.to(cpudev), perturbed_output.to(cpudev)
@@ -47,5 +48,47 @@ def fgsm_targeted(model, image_bytes, epsilon, target, device=torch.device('cpu'
 	perturbed_image = rev_transform(perturbed_image[0])
 	original_image = rev_transform(tensor[0])
 	perturbation = rev_transform(per_tensor[0])
+	del model
 	return dict(top5_original), dict(top5_perturbed), perturbed_image, original_image, perturbation
 
+def basic_iterative(model_inp, image_bytes, alpha, epsilon, num_iter, device=torch.device('cpu')):
+	model = get_model(model_inp)
+	tensor = transform_image(image_bytes=image_bytes)
+	#outputs = model.forward(tensor)
+	original_output, perturbed_output, perturbed_image = GAE.basic_iterative(model, tensor, alpha, epsilon, num_iter, device)
+	cpudev = torch.device('cpu')
+	original_output, perturbed_output = original_output.to(cpudev), perturbed_output.to(cpudev)
+	perturbed_image, tensor = perturbed_image.to(cpudev), tensor.to(cpudev)
+	per_tensor = perturbed_image - tensor
+	or_percentage = torch.nn.functional.softmax(original_output, dim=1)[0] * 100
+	_, or_indices = torch.sort(original_output, descending=True)
+	per_percentage = torch.nn.functional.softmax(perturbed_output, dim=1)[0] * 100
+	_, per_indices = torch.sort(perturbed_output, descending=True)
+	top5_original = [(imagenet_class_index[str(idx.item())][1], or_percentage[idx.item()].item()) for idx in or_indices[0][:5]]
+	top5_perturbed = [(imagenet_class_index[str(idx.item())][1], or_percentage[idx.item()].item()) for idx in per_indices[0][:5]]
+	perturbed_image = rev_transform(perturbed_image[0])
+	original_image = rev_transform(tensor[0])
+	perturbation = rev_transform(per_tensor[0])
+	del model
+	return dict(top5_original), dict(top5_perturbed), perturbed_image, original_image, perturbation
+
+def iterative_ll_class(model_inp, image_bytes, alpha, epsilon, num_iter, device=torch.device('cpu')):
+	model = get_model(model_inp)
+	tensor = transform_image(image_bytes=image_bytes)
+	#outputs = model.forward(tensor)
+	original_output, perturbed_output, perturbed_image = GAE.basic_iterative(model, tensor, alpha, epsilon, num_iter, device)
+	cpudev = torch.device('cpu')
+	original_output, perturbed_output = original_output.to(cpudev), perturbed_output.to(cpudev)
+	perturbed_image, tensor = perturbed_image.to(cpudev), tensor.to(cpudev)
+	per_tensor = perturbed_image - tensor
+	or_percentage = torch.nn.functional.softmax(original_output, dim=1)[0] * 100
+	_, or_indices = torch.sort(original_output, descending=True)
+	per_percentage = torch.nn.functional.softmax(perturbed_output, dim=1)[0] * 100
+	_, per_indices = torch.sort(perturbed_output, descending=True)
+	top5_original = [(imagenet_class_index[str(idx.item())][1], or_percentage[idx.item()].item()) for idx in or_indices[0][:5]]
+	top5_perturbed = [(imagenet_class_index[str(idx.item())][1], or_percentage[idx.item()].item()) for idx in per_indices[0][:5]]
+	perturbed_image = rev_transform(perturbed_image[0])
+	original_image = rev_transform(tensor[0])
+	perturbation = rev_transform(per_tensor[0])
+	del model
+	return dict(top5_original), dict(top5_perturbed), perturbed_image, original_image, perturbation
